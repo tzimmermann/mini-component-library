05ce696a2394a704bb1fa00fa48dc54b8b0648db
Thomas Zimmermann <thomas.zimmermann@signavio.com>
2022-11-02 08:59:50 +0100

    feat: getting started with IconInput
    - still missing: label, focus & hover states

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const IconInput = ({
   label,
   icon,
   width = 250,
-  size,
+  size = "small",
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+    <InputWrapper
+      style={{
+        "--width": `${width}px`,
+        "--paddingBottom": `${size === "small" ? 2 : 4}px`,
+      }}
+    >
+      <Icon
+        id={icon}
+        size={size === "small" ? 14 : 20}
+        strokeWidth={size === "small" ? 1 : 2}
+      />
+      <StyledInput
+        type="text"
+        placeholder={placeholder}
+        style={{
+          "--fontSize": `${size === "small" ? 14 : 18}px`,
+        }}
+      />
+    </InputWrapper>
+  );
 };
+
+const InputWrapper = styled.span`
+  display: flex;
+  align-items: center;
+  width: var(--width);
+  border-bottom: 1px solid ${COLORS.black};
+  padding-bottom: var(--paddingBottom);
+  color: ${COLORS.gray700};
+
+  & ::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  flex: 1 auto;
+  padding-left: 10px;
+  color: ${COLORS.gray700};
+  font-size: var(--fontSize);
+  font-weight: 700;
+`;
 
 export default IconInput;
